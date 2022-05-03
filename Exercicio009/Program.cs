af94be08f2d4d9d2d614996e45657defcca4570b
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-05-02 21:59:50 -0300

    exercicio 9 - dia 02-05-2022

---
+++ Exercicio009/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Efetuar a conversão de temperatura da escala Celsius para a escala Fahrenheit. 
+ **/
+
+// declaração das variaveis do tipo double para o recebimento dos valores e calculos
+double celsius, fahr;
+
+// imprime a mensagem na tela e lê o valor digitado pelo usuario
+Console.Write("Digite o valor da temperatura em ºC: ");
+celsius = double.Parse(Console.ReadLine());
+
+// calcula a conversão de celsius para fahrenheit
+fahr = 1.80 * celsius + 32;
+
+
+// também pode ser calculado assim
+// fahr = celsius * ((double)9 / 5) + 32;
+
+// imprime na tela o resultado da conversão com uma frase
+Console.WriteLine("A temperatura de {0:F2}ºC se torna {1:F2}ºF", celsius, fahr);
